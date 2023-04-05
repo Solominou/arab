cc48cf5309e56f68f78748f31da1faf902786349
solominou <96089086+Solominou@users.noreply.github.com>
2023-04-05 16:36:13 +0300

    Update build.gradle.kts

--- Shahid4uProvider/build.gradle.kts
+++ Shahid4uProvider/build.gradle.kts
@@ -10,5 +10,5 @@ cloudstream {
 
     tvTypes = listOf( "TvSeries" , "Movie" , "Anime" , "AsianDrama" )
 
-    iconUrl = "https://www.google.com/s2/favicons?domain=shahid4u.cc&sz=%size%"
+    iconUrl = "https://www.google.com/s2/favicons?domain=shahid4uu.cam&sz=%size%"
 }
