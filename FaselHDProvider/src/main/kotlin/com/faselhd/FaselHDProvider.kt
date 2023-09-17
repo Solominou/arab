61c395866e6160db8a255e4eb613b113bba4e654
solominou <96089086+Solominou@users.noreply.github.com>
2023-09-17 10:07:52 +0300

    Update FaselHDProvider.kt

--- FaselHDProvider/src/main/kotlin/com/faselhd/FaselHDProvider.kt
+++ FaselHDProvider/src/main/kotlin/com/faselhd/FaselHDProvider.kt
@@ -1,6 +1,5 @@
 package com.faselhd
 
-
 import com.lagradost.cloudstream3.*
 import com.lagradost.cloudstream3.utils.ExtractorLink
 import com.lagradost.nicehttp.requestCreator
@@ -13,12 +12,12 @@ import org.jsoup.nodes.Element
 class FaselHD : MainAPI() {
     override var lang = "ar"
     override var mainUrl = "https://faselhd.io"
-    private  val alternativeUrl = "https://www.faselhd.club"
+    private val alternativeUrl = "https://www.faselhd.club"
     override var name = "FaselHD"
     override val usesWebView = false
     override val hasMainPage = true
     override val supportedTypes = setOf(TvType.TvSeries, TvType.Movie, TvType.AsianDrama, TvType.Anime)
-    private  val cfKiller = CloudflareKiller()
+    private val cfKiller = CloudflareKiller()
 
     private fun String.getIntFromText(): Int? {
         return Regex("""\d+""").find(this)?.groupValues?.firstOrNull()?.toIntOrNull()
@@ -26,13 +25,13 @@ class FaselHD : MainAPI() {
 
     private fun Element.toSearchResponse(): SearchResponse? {
         val url = select("div.postDiv a").attr("href") ?: return null
-        val posterUrl = select("div.postDiv a div img").attr("data-src") ?:
-        select("div.postDiv a div img").attr("src")
+        val posterUrl = select("div.postDiv a div img").attr("data-src")
+            ?: select("div.postDiv a div img").attr("src")
         val title = select("div.postDiv a div img").attr("alt")
         val quality = select(".quality").first()?.text()?.replace("1080p |-".toRegex(), "")
-        val type = if(title.contains("فيلم")) TvType.Movie else TvType.TvSeries
+        val type = if (title.contains("فيلم")) TvType.Movie else TvType.TvSeries
         return MovieSearchResponse(
-            title.replace("الموسم الأول|برنامج|فيلم|مترجم|اون لاين|مسلسل|مشاهدة|انمي|أنمي".toRegex(),""),
+            title.replace("الموسم الأول|برنامج|فيلم|مترجم|اون لاين|مسلسل|مشاهدة|انمي|أنمي".toRegex(), ""),
             url,
             this@FaselHD.name,
             type,
@@ -43,19 +42,20 @@ class FaselHD : MainAPI() {
             posterHeaders = cfKiller.getCookieHeaders(alternativeUrl).toMap()
         )
     }
-    override val mainPage = mainPageOf(
-            "$mainUrl/all-movies/page/" to  "الافلام",
-            "$mainUrl/movies_top_views/page/" to "الافلام الاعلي مشاهدة",
-            "$mainUrl/dubbed-movies/page/" to "الأفلام المدبلجة",
-            "$mainUrl/movies_top_imdb/page/" to "الافلام الاعلي تقييما IMDB",
-            "$mainUrl/series/page/" to "مسلسلات",
-            "$mainUrl/recent_series/page/" to "المضاف حديثا",
-            "$mainUrl/anime/page/" to "الأنمي",
-        )
 
-    override suspend fun getMainPage(page: Int, request : MainPageRequest): HomePageResponse {
+    override val mainPage = mainPageOf(
+        "$mainUrl/all-movies/page/" to "الافلام",
+        "$mainUrl/movies_top_views/page/" to "الافلام الاعلي مشاهدة",
+        "$mainUrl/dubbed-movies/page/" to "الأفلام المدبلجة",
+        "$mainUrl/movies_top_imdb/page/" to "الافلام الاعلي تقييما IMDB",
+        "$mainUrl/series/page/" to "مسلسلات",
+        "$mainUrl/recent_series/page/" to "المضاف حديثا",
+        "$mainUrl/anime/page/" to "الأنمي",
+    )
+
+    override suspend fun getMainPage(page: Int, request: MainPageRequest): HomePageResponse {
         var doc = app.get(request.data + page).document
-        if(doc.select("title").text() == "Just a moment...") {
+        if (doc.select("title").text() == "Just a moment...") {
             doc = app.get(request.data.replace(mainUrl, alternativeUrl) + page, interceptor = cfKiller, timeout = 120).document
         }
         val list = doc.select("div[id=\"postList\"] div[class=\"col-xl-2 col-lg-2 col-md-3 col-sm-3\"]")
@@ -66,9 +66,9 @@ class FaselHD : MainAPI() {
     }
 
     override suspend fun search(query: String): List<SearchResponse> {
-        val q = query.replace(" ","+")
+        val q = query.replace(" ", "+")
         var d = app.get("$mainUrl/?s=$q").document
-        if(d.select("title").text() == "Just a moment...") {
+        if (d.select("title").text() == "Just a moment...") {
             d = app.get("$alternativeUrl/?s=$q", interceptor = cfKiller, timeout = 120).document
         }
         return d.select("div[id=\"postList\"] div[class=\"col-xl-2 col-lg-2 col-md-3 col-sm-3\"]")
@@ -77,10 +77,9 @@ class FaselHD : MainAPI() {
             }
     }
 
-
     override suspend fun load(url: String): LoadResponse {
         var doc = app.get(url).document
-        if(doc.select("title").text() == "Just a moment...") {
+        if (doc.select("title").text() == "Just a moment...") {
             doc = app.get(url, interceptor = cfKiller, timeout = 120).document
         }
         val isMovie = doc.select("div.epAll").isEmpty()
@@ -93,7 +92,7 @@ class FaselHD : MainAPI() {
 
         val title =
             doc.select("title").text().replace(" - فاصل إعلاني", "")
-                .replace("الموسم الأول|برنامج|فيلم|مترجم|اون لاين|مسلسل|مشاهدة|انمي|أنمي|$year".toRegex(),"")
+                .replace("الموسم الأول|برنامج|فيلم|مترجم|اون لاين|مسلسل|مشاهدة|انمي|أنمي|$year".toRegex(), "")
         // A bit iffy to parse twice like this, but it'll do.
         val duration = doc.select("div[id=\"singleList\"] div[class=\"col-xl-6 col-lg-6 col-md-6 col-sm-6\"]").firstOrNull {
             it.text().contains("مدة|توقيت".toRegex())
@@ -134,11 +133,11 @@ class FaselHD : MainAPI() {
                 )
             }
             doc.select("div[id=\"seasonList\"] div[class=\"col-xl-2 col-lg-3 col-md-6\"] div.seasonDiv")
-                .not(".active").apmap { it ->
-					val id = it.attr("onclick").replace(".*\\/\\?p=|'".toRegex(), "")
-                    var s = app.get("$mainUrl/?p="+id).document
-                    if(s.select("title").text() == "Just a moment...") {
-                        s = app.get("$alternativeUrl/?p="+id, interceptor = cfKiller).document
+                .not(".active").forEach { it ->
+                    val id = it.attr("onclick").replace(".*\\/\\?p=|'".toRegex(), "")
+                    var s = app.get("$mainUrl/?p=" + id).document
+                    if (s.select("title").text() == "Just a moment...") {
+                        s = app.get("$alternativeUrl/?p=" + id, interceptor = cfKiller).document
                     }
                     s.select("div.epAll a").map {
                         episodes.add(
@@ -163,46 +162,4 @@ class FaselHD : MainAPI() {
         }
     }
 
-    override suspend fun loadLinks(
-        data: String,
-        isCasting: Boolean,
-        subtitleCallback: (SubtitleFile) -> Unit,
-        callback: (ExtractorLink) -> Unit
-    ): Boolean {
-        var doc = app.get(data).document
-        if(doc.select("title").text() == "Just a moment...") {
-            doc = app.get(data, interceptor = cfKiller).document
-        }
-        listOf(
-            doc.select(".downloadLinks a").attr("href") to "download",
-            doc.select("iframe[name=\"player_iframe\"]").attr("src") to "iframe"
-        ).apmap { (url, method) ->
-            if(method == "download") {
-                val player = app.post(url, interceptor = cfKiller, referer = mainUrl, timeout = 120).document
-                callback.invoke(
-                    ExtractorLink(
-                        this.name,
-                        this.name + " Download Source",
-                        player.select("div.dl-link a").attr("href"),
-                        this.mainUrl,
-                        Qualities.Unknown.value
-                    )
-                )
-            } else if(method == "iframe") {
-                val webView = WebViewResolver(
-                    Regex("""master\.m3u8""")
-                ).resolveUsingWebView(
-                    requestCreator(
-                        "GET", url, referer = mainUrl
-                    )
-                ).first
-                M3u8Helper.generateM3u8(
-                    this.name,
-                    webView?.url.toString(),
-                    referer = mainUrl
-                ).toList().forEach(callback)
-            }
-        }
-        return true
-    }
-}
+    override suspend
