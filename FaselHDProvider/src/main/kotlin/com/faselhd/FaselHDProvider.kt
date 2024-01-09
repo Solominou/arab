3150e19d39cd460e1daa31f8e9823501a64b2f4c
solominou <96089086+Solominou@users.noreply.github.com>
2024-01-09 14:52:12 +0200

    Update FaselHDProvider.kt

--- FaselHDProvider/src/main/kotlin/com/faselhd/FaselHDProvider.kt
+++ FaselHDProvider/src/main/kotlin/com/faselhd/FaselHDProvider.kt
@@ -25,9 +25,9 @@ class FaselHD : MainAPI() {
 
     private fun Element.toSearchResponse(): SearchResponse? {
         val url = select("div.postDiv a").attr("href") ?: return null
-        val posterUrl = select("div.postDiv a div img").attr("data-src")
-            ?: select("div.postDiv a div img").attr("src")
-        val title = select("div.postDiv a div img").attr("alt")
+        val posterUrl = select("div.postDiv a imgdiv-class img").attr("data-src")
+            ?: select("div.postDiv a imgdiv-class img").attr("src")
+        val title = select("div.postDiv a imgdiv-class img").attr("alt")
         val quality = select(".quality").first()?.text()?.replace("1080p |-".toRegex(), "")
         val type = if (title.contains("فيلم")) TvType.Movie else TvType.TvSeries
         return MovieSearchResponse(
