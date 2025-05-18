5f8c2314c39b9eccae0a72c134e4c46456730b4b
solominou <96089086+Solominou@users.noreply.github.com>
2025-05-18 03:36:29 +0300

    Add files via upload

---
+++ FaselHD/build.gradle.kts
@@ -0,0 +1,10 @@
+
+plugins {
+    id("cloudstream-plugin")
+}
+
+cloudstream {
+    language.set("ar")
+    description.set("Arabic movie and series provider")
+    authors.set(listOf("Solominou"))
+}
