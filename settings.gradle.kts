94062929220121a212c574648166387d5c487a9c
solominou <96089086+Solominou@users.noreply.github.com>
2025-05-18 03:15:27 +0300

    Add files via upload

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,13 +1,1 @@
-rootProject.name = "CloudstreamExtensionsArabic"
-
-val disabled = listOf<String>("BestProvider")
-
-File(rootDir, ".").eachDir { dir ->
-    if (!disabled.contains(dir.name) && File(dir, "build.gradle.kts").exists()) {
-        include(dir.name)
-    }
-}
-
-fun File.eachDir(block: (File) -> Unit) {
-    listFiles()?.filter { it.isDirectory }?.forEach { block(it) }
-}
+rootProject.name = "FaselHD"
